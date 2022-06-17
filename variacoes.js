6593127877f7caf50d60b97c34d4429ae2a34a77
douglas <douglaslinharespe@gmail.com>
2022-06-17 12:30:21 -0300

    variacoes

---
+++ variacoes.js
@@ -0,0 +1,21 @@
+//Tipos de variações 
+//Nome: Corrida
+
+let Atletas = ["Novos", "Velhos"]
+if (Atletas.length <= 3)
+{ 
+    console.log("são aptos a participação.")
+}
+
+let NumerosDeInscritos = 300
+
+if (NumerosDeInscritos < 400 )
+{
+    console.log("inscrição feita com sucesso!")
+}
+
+else 
+{
+    console.log("As inscrições foram encerradas não há vagas!!")
+}
+
